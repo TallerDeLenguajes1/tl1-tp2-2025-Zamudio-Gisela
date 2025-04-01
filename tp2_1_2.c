85767e2af75197a711f1836065e5dafca512dc43
Zamudio Gisela <zagarogi@gmail.com>
2025-04-01 15:57:24 -0300

    copia de punto 1 en punto 2

--- tp2_1_2.c
+++ tp2_1_2.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
- int main() {
+#define N 20
 
-    printf("efewfe");
+int main() {
 
-    return 0;
- }
+   int i;
+   double vt[N];
+   
+   for (int i = 0; i < N; i++)
+   {
+      vt[i] = 1+rand()%100;
+   }
+   
+   printf("%f -", vt[i]);
+
+   return 0;
+}
